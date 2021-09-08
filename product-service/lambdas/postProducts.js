e43e44c1b85dc6111d3f269554adb6af4941f8a7
Vadimir547 <germes547@gmail.com>
2021-09-08 23:26:28 +0300

    feat: task 4 add post create

---
+++ product-service/lambdas/postProducts.js
@@ -0,0 +1,69 @@
+import products from './db/products.json';
+import { Client } from 'pg';
+import { ConnectDB, dbOptions } from '../conection'
+
+export const handler = async event => {
+  console.log(event);
+  const db = new ConnectDB(dbOptions);
+  const client = await db.connect();
+
+  try {
+    const { title, description, price, count } = JSON.parse(event.body);
+    const notAllParams = !title || !description || !price  || !count;
+
+    if (notAllParams) {
+      return {
+        headers: {
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Methods': '*',
+                'Access-Control-Allow-Origin': '*',
+              },
+            statusCode: 400,
+            body: JSON.stringify({ message: 'lack params' })
+        };
+    }
+
+    await client.query('BEGIN');
+
+    const insertSql =
+      'INSERT INTO products(title, description, price) VALUES($1, $2, $3) RETURNING id';
+    const postQuerry = await client.query(insertSql, [title, description, price]);
+    const { id: productId } = postQuerry.rows[0];
+
+    const StocksSql = 'INSERT INTO stock(product_id, count) VALUES ($1, $2)';
+    await client.query(StocksSql, [productId, count]);
+
+    await client.query('COMMIT');
+
+    const dataToResponse = {
+      title,
+      description,
+      price,
+      count,
+      id: productId,
+    };
+
+return  {       
+        headers: {
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Methods': '*',
+                'Access-Control-Allow-Origin': '*',
+            },
+            statusCode: 400,
+            body: JSON.stringify(dataToResponse)
+        };
+  } catch (error) {
+    await client.query('ROLLBACK');
+    return {       
+        headers: {
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Methods': '*',
+                'Access-Control-Allow-Origin': '*',
+            },
+            statusCode: 500,
+            body: JSON.stringify(error.message)
+        };
+  } finally {
+    await db.disconnect();
+  }
+};
